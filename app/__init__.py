6469e5173de52508c4dea6510d9f91b22de65ab8
preisj <58543036+preisj@users.noreply.github.com>
2025-07-07 17:35:05 -0300

    Test deploy staging

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,16 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from .config import Config
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///local.db")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config.from_object(Config)
+
     db.init_app(app)
 
-    with app.app_context():
-        from app import models
-        db.create_all()
+    from .routes import main
+    app.register_blueprint(main)
 
-    from app.routes import task_bp
-    app.register_blueprint(task_bp)
     return app
